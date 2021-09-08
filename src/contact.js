00b21f7e21caff2812f1d265aad097334a385347
Katherine Burr <katherinevb@hotmail.co.uk>
2021-09-08 18:00:40 +0100

    Fade in on each tab change

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,7 @@
 
 export default function Contact() {
     const contact = document.createElement('div');
+    contact.classList.add('contact');
     const head = document.createElement('h3');
     head.innerText = 'CONTACT PAGE';
     contact.appendChild(head);
--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,7 @@ import Beer from './beer.jpeg'
 
 export default function Menu() {
     const menu = document.createElement('div');
+    menu.classList.add('menu');
     const head = document.createElement('h3');
     head.innerText = 'MENU';
     menu.appendChild(head)
