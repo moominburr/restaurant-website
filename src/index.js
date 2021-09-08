912c77312d2e093cec7c5c5e5085929b4529a8b5
Katherine Burr <katherinevb@hotmail.co.uk>
2021-09-08 17:55:35 +0100

    Active tab styling

--- src/index.js
+++ src/index.js
@@ -14,18 +14,37 @@ function page() {
   element.appendChild(heading);
   element.appendChild(Nav());
   const main = document.createElement("main");
+  main.appendChild(Home());
   const homeBtn = element.querySelector("#home");
   const menuBtn = element.querySelector("#menu");
   const contactBtn = element.querySelector("#contact");
+  const btns = element.querySelectorAll('.nav-li');
+  
   homeBtn.addEventListener("click", () => {
+    homeBtn.classList.add('nav-li-active');
+    btns.forEach(btn => {
+      if(btn.innerText === 'home') return;
+      btn.classList.contains('nav-li-active') ? btn.classList.remove('nav-li-active') : '';
+    });
     main.innerHTML = "";
     main.appendChild(Home());
   });
   menuBtn.addEventListener("click", () => {
+    menuBtn.classList.add('nav-li-active');
+    btns.forEach(btn => {
+      if(btn.innerText === 'menu') return;
+      btn.classList.contains('nav-li-active') ? btn.classList.remove('nav-li-active') : '';
+    });
     main.innerHTML = "";
     main.appendChild(Menu());
   });
   contactBtn.addEventListener("click", () => {
+    contactBtn.classList.add('nav-li-active');
+    console.log(contactBtn);
+    btns.forEach(btn => {
+      if(btn.innerText === 'contact') return;
+      btn.classList.contains('nav-li-active') ? btn.classList.remove('nav-li-active') : '';
+    });
     main.innerHTML = "";
     main.appendChild(Contact());
   });
