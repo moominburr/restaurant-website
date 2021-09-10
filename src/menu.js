9de410155090d082e4570d7e1e07a85620751ca5
Katherine Burr <katherinevb@hotmail.co.uk>
2021-09-10 12:11:13 +0100

    Data for menu page created and styling added

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,29 @@
 import Beer from './beer.jpeg'
+import menuItemsList from './menuItems';
 
 export default function Menu() {
     const menu = document.createElement('div');
     menu.classList.add('menu');
     const head = document.createElement('h3');
-    head.innerText = 'MENU';
-    menu.appendChild(head)
-    
+    head.innerText = 'SUMMER MENU';
+    menu.appendChild(head);
+    const menuCard = document.createElement('div');
+    menuCard.classList.add('menu-card');
+    let menuItems = menuItemsList();
+    menuItems.forEach(item => {
+      const itemCard = document.createElement('div');
+      itemCard.classList.add('item-card');
+      const name = document.createElement('h4');
+      name.innerText = item.name;
+      itemCard.appendChild(name);
+      const description = document.createElement('p');
+      description.innerText = item.description;
+      itemCard.appendChild(description);
+      const price = document.createElement('h5');
+      price.innerText = `£${item.price}`;
+      itemCard.appendChild(price);
+      menuCard.appendChild(itemCard); 
+    });
+    menu.appendChild(menuCard);
     return menu;
   }
---
+++ src/menuItems.js
@@ -0,0 +1,53 @@
+export default function menuItems() {
+  return [
+    {
+      name: "English Brunch",
+      description:
+        "Sausages, bacon, hash browns, tomatoes, mushrooms and eggs, served the English Way (fried and delicious!)",
+      price: "11.99",
+    },
+    {
+      name: "American Pancakes",
+      description:
+        "Big, fluffy pancakes with strawberries and blueberries served with your choice of chocolate, strawberry or maple sauce",
+      price: "8.99",
+    },
+    {
+      name: "Berries and granola",
+      description: "Sweet and crunchy granola with oaty flakes, mixed nuts and seasonal berries",
+      price: "5.99",
+    },
+    {
+      name: "Banana Pancakes",
+      description:
+        "Two ingredient pancakes - banana and egg - topped with dried banana flakes, whipped cream and chocolate sprinkles",
+      price: "6.99",
+    },
+    {
+      name: "Crossiant Platter",
+      description:
+        "All of the crossiants! Crescent shaped, pain au chocolat, pain au raisin and almond crossiants. Best for sharers",
+      price: "12.99",
+    },
+    {
+      name: "Eggs Royale",
+      description: "Poached eggs on English muffins with oak smoked salmon and hollandaise sauce",
+      price: "8.99",
+    },
+    {
+      name: "Belgian Blonde",
+      description: "Golden and smooth",
+      price: "3.99",
+    },
+    {
+      name: "Belgian Dark",
+      description: "Earthy and tasty",
+      price: "3.99",
+    },
+    {
+      name: "IPA",
+      description: `One of the best IPAs around`,
+      price: "4.29",
+    },
+  ];
+}
